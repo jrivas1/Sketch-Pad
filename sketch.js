13f988817e67f4e86c33bbb277b8d8233a38c0c6
Joey <130940184+jrivas1@users.noreply.github.com>
2023-04-20 15:23:55 -0400

    add size change and click drag effect

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
+//Globals
+
+
 const container = document.querySelector(".container");
+const sizeButton = document.querySelector("#sizeButton");
 renderGrid(16, 16);
 
 function renderGrid(height, width) {
+    if (!Number.isInteger(height) || !Number.isInteger(width)) {
+        console.warn(`Invalid height and width! Inputs: ${width}, ${height}`)
+        return;
+    }
+    let rows = [];
     for (let i = 0; i < width; i++) {
         let row = document.createElement("div");
         row.classList.add("row");
@@ -10,6 +19,32 @@ function renderGrid(height, width) {
             newBox.classList.add("cell");
             row.appendChild(newBox);
         }
-        container.appendChild(row);
+        rows.push(row);
     }
+    container.replaceChildren(...rows);
 }
+
+function paintCell(e) {
+    if (e.target.classList.contains("cell")){
+        e.target.classList.add("painted");
+    }
+    e.preventDefault();
+}
+
+container.addEventListener("mousedown", e => {
+    paintCell(e);
+    container.addEventListener("mouseover", paintCell);
+});
+
+container.addEventListener("mouseup", e => {
+    container.removeEventListener("mouseover", paintCell);
+});
+container.addEventListener("mouseleave", e => {
+    container.removeEventListener("mouseover", paintCell);
+});
+
+sizeButton.addEventListener("click", () => {
+    let width = prompt("Width?");
+    let height = prompt("Height?");
+    renderGrid(width, height);
+})
